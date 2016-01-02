a1a1b168d040dcfd610bcf9d34018d38569205e1
Tanishq Dubey <tanishq.dubey@gmail.com>
2016-01-02 13:55:41 -0600

    Initial Commit

---
+++ DJ Khaled Quotes/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DJ Khaled Quotes
+//
+//  Created by Tanishq Dubey on 1/2/16.
+//  Copyright © 2016 TDApps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
