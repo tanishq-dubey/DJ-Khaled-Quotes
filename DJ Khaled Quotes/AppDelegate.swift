4591545fefafca40fe08b7d501332653d715c391
tommypacker <tommypacker@gmail.com>
2016-01-02 22:41:30 -0800

    Add more Khaled quotes

--- DJ Khaled Quotes/AppDelegate.swift
+++ DJ Khaled Quotes/AppDelegate.swift
@@ -13,18 +13,26 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     
     var quotes = ["Another One.",
                   "Congratulations. You played yourself",
-                   "Ride wid me",
+                   "Ride wit me through the pathway to more success",
                    "Smh they get mad when u have joy.",
                    "I can deal with everything. I got the answer for anything. This DJ Khaled.",
                    "I didn’t want to be an artist in the same place I worked. You could always have people that might feel like you would do something more for yourself.",
                     "When I made ‘Hold You Down,’ I was thinking Barry Gordy, Quincy Jones. I was saying to myself, ‘If they was in the studio, what was they thinking.’ So I started thinking DJ Khaled, but inspired by the greats.",
                     "My fans expect me to be greater and keep being great.",
-                    "The key to more success is coco butter.",
+                    "The key to more success is cocoa butter.",
                     "Those that weather the storm r the great ones.",
                     "They don’t want you to win. They don’t want you to have the No. 1 record in the country. They don’t want you get healthy. They don’t want you to exercise. And they don’t want you to have that view.",
                     "I wanted to see what Jay Z was looking through his window, if he even had a window.",
-                    "I can deal with everything. I got the answer for   anything. This DJ Khaled.",
-                    "I changed… a lot."]
+                    "I can deal with everything. I got the answer for anything. This DJ Khaled.",
+                    "I changed… a lot.", "It's so real out here right now, the only reason why you see anything is cause I got the flash on.", "Bless up.",
+                    "I be confused. Why they be mad?", "Enjoy life man, live it up", "Liooonnnn", "Almond milk + Cinamon Toast Crunch. Major key to success",
+                    "Some people can't handle success. I can.", "They don't want you to be on a jet ski doing 360s.", "Some people can't handle winning. I can.",
+                    "The key is not to drive your jetski in the dark", "Its important to shape up your hedges. Its like getting a haircut", "Cloth talk",
+                    "Let's see what Chef Dee got for breakfast", "Egg whites, chicken sausage, water. They don't want you to eat", "Don't play yourself",
+                    "You have to make it through the jungle to make it to paradise. That's the key.", "The key to success is to have a hammock",
+                    "They wanna come stress me out. Heh, bye.", "All the great ones jetski", "They don't want you to have a rolls royce. I promise",
+                    "Do you see that bamboo? Ain't nothing like bamboo.", "We have to get money. We have no choice. It cost money to eat", "The key is to have every key",
+                    "I got a starfruit tree. Star fruit is one of my favorite fruits"]
 
     @IBOutlet weak var window: NSWindow!
     
